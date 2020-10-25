df5261b9edae121621d6430fd7fc94b42f9bad40
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-10-25 17:44:13 -0500

    Add Components

---
+++ react-gallery-app/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import SearchForm from './SearchForm'
+import Nav from './Nav'
+import PhotoContainer from './PhotoContainer'
+
+function App() {
+  return (
+    <div className="container">
+      <SearchForm />
+      <Nav />
+      <PhotoContainer />
+    </div>
+  );
+}
+
+export default App;
