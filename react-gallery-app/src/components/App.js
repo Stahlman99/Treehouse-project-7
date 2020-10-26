9e3d2ffcdfc309ee48d0a14dff5232a0fa6ca299
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-10-25 19:39:47 -0500

    Add Routing

--- react-gallery-app/src/components/App.js
+++ react-gallery-app/src/components/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+
 import '../App.css';
 import SearchForm from './SearchForm'
 import Nav from './Nav'
 import PhotoContainer from './PhotoContainer'
+import apiKey from '../config.js';
+import NotFound from './NotFound'
 
 function App() {
   return (
-    <div className="container">
-      <SearchForm />
-      <Nav />
-      <PhotoContainer />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <SearchForm />
+        <Nav />
+        <Switch>
+          <Route exact path="/" render={() => <PhotoContainer apiKey={apiKey}/>} query=""/>
+          <Route path="/cats" render={() => <PhotoContainer apiKey={apiKey} query="cats"/>}/>
+          <Route path="/dogs" render={() => <PhotoContainer apiKey={apiKey} query="dogs"/>}/>
+          <Route path="/computers" render={() => <PhotoContainer apiKey={apiKey} query="computers"/>}/>
+          <Route path="/search" render={() => <PhotoContainer apiKey={apiKey}/>} query=""/>
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
