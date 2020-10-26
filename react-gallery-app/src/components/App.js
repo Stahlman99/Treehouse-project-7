f89d5a857e367b83d2f98cd6531d2b71fd34c135
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-10-25 22:25:34 -0500

    Add basic data fetching

--- react-gallery-app/src/components/App.js
+++ react-gallery-app/src/components/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter,
   Route,
   Switch
 } from 'react-router-dom';
-
+import axios from 'axios'
 
 import '../App.css';
 import SearchForm from './SearchForm'
@@ -13,23 +13,59 @@ import PhotoContainer from './PhotoContainer'
 import apiKey from '../config.js';
 import NotFound from './NotFound'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="container">
-        <SearchForm />
-        <Nav />
-        <Switch>
-          <Route exact path="/" render={() => <PhotoContainer apiKey={apiKey}/>} query=""/>
-          <Route path="/cats" render={() => <PhotoContainer apiKey={apiKey} query="cats"/>}/>
-          <Route path="/dogs" render={() => <PhotoContainer apiKey={apiKey} query="dogs"/>}/>
-          <Route path="/computers" render={() => <PhotoContainer apiKey={apiKey} query="computers"/>}/>
-          <Route path="/search" render={() => <PhotoContainer apiKey={apiKey}/>} query=""/>
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      catPics: [],
+      dogPics: [],
+      computerPics: []
+    }
+  }
+  
+
+  fetchData = (query) => {
+    let response;
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+    .then(res => {
+      response = res.data.photos.photo;
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    });
+
+    return response
+  }
+
+  componentDidMount() {
+    this.setState({
+      catPics: this.fetchData('cat'),
+      dogPics: this.fetchData('dog'),
+      computerPics: this.fetchData('computer')
+    });
+  }
+
+  render() {
+    console.log(this.state.catPics);
+    
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <SearchForm />
+          <Nav />
+          <Switch>
+            <Route exact path="/" render={() => <PhotoContainer />}/>
+            <Route path="/cats" render={() => <PhotoContainer data={this.state.catPics}/>}/>
+            <Route path="/dogs" render={() => <PhotoContainer data={this.state.dogPics}/>}/>
+            <Route path="/computers" render={() => <PhotoContainer data={this.state.computerPics}/>}/>
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+
 }
 
 export default App;
