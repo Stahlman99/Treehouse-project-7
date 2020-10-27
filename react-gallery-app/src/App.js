bd200ac98ae5138213aa5c193e863b85ca897f0e
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-10-27 13:08:29 -0500

    Add safe search to the Flickr API request

--- react-gallery-app/src/App.js
+++ react-gallery-app/src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   
   // This method sets the values of the three default search options when the component mounts.
   componentDidMount() {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&sort=relevance&per_page=24&format=json&nojsoncallback=1`)
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&sort=relevance&safe_search=1&per_page=24&format=json&nojsoncallback=1`)
     .then(res => {
       this.setState({
         catPics: res.data.photos.photo
@@ -42,7 +42,7 @@ class App extends Component {
       console.log('Error fetching and parsing data', error);
     });
 
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=dogs&sort=relevance&per_page=24&format=json&nojsoncallback=1`)
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=dogs&sort=relevance&safe_search=1&per_page=24&format=json&nojsoncallback=1`)
     .then(res => {
       this.setState({
         dogPics: res.data.photos.photo
@@ -52,7 +52,7 @@ class App extends Component {
       console.log('Error fetching and parsing data', error);
     });
 
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=computer&sort=relevance&per_page=24&format=json&nojsoncallback=1`)
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=computer&sort=relevance&safe_search=1&per_page=24&format=json&nojsoncallback=1`)
     .then(res => {
       this.setState({
         computerPics: res.data.photos.photo
@@ -65,7 +65,7 @@ class App extends Component {
 
   // This method fetches the data for a custom query.
   performSearch = (query) => {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&sort=relevance&per_page=24&format=json&nojsoncallback=1`)
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&sort=relevance&safe_search=1&per_page=24&format=json&nojsoncallback=1`)
     .then(res => {
       this.setState({
         searchRes: res.data.photos.photo
